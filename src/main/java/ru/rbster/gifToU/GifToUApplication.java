1e432ba68325e112fc7955076256a5c6724d081d
Rbster <Rbster@yandex.ru>
2021-09-02 16:07:15 +0300

    Initial commit

---
+++ src/main/java/ru/rbster/gifToU/GifToUApplication.java
@@ -0,0 +1,13 @@
+package ru.rbster.gifToU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GifToUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GifToUApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/rbster/gifToU/GifToUApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rbster.gifToU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GifToUApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
